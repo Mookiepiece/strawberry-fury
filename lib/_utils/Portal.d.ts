3d10303a5c62e771a42545c311dc0f18526e8afe
彼術向 <48076971+Mookiepiece@users.noreply.github.com>
2021-06-19 18:05:53 +0800

    Updates

--- lib/_utils/Portal.d.ts
+++ lib/_utils/Portal.d.ts
@@ -2,3 +2,4 @@ import React from 'react';
 export declare const setup: (key: 'Modal' | 'Notification') => HTMLDivElement;
 export declare const Portal: React.FC;
 export declare const NotificationPortal: React.FC;
+export declare const useMountedStatePlus: () => boolean;
